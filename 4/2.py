b4a93b890753005ab453acd8829101c26143d349
Andrew Han <awhan@wpi.edu>
2021-12-08 03:11:46 -0500

    Complete day 4

---
+++ 4/2.py
@@ -0,0 +1,49 @@
+import sys
+
+class Board:
+    def __init__(self, lines):
+        self.bingo = {number:{'x':i, 'y':j, 'marked':False}
+                for i, line in enumerate(lines)
+                for j, number in enumerate(line.split())}
+
+    def CheckWin(self, x, y):
+        if len(list(filter(lambda number: number['x'] == x and 
+            number['marked'], self.bingo.values()))) == 5:
+                return True
+        elif len(list(filter(lambda number: number['y'] == y and 
+            number['marked'], self.bingo.values()))) == 5:
+                return True
+        else:
+            return False
+
+    def Mark(self, number):
+        if number not in self.bingo.keys():
+            return False
+        else:
+            self.bingo[number]['marked'] = True
+            return self.CheckWin(self.bingo[number]['x'],
+                self.bingo[number]['y'])
+
+    def GetScore(self, number):
+        unmarked = [int(x[0]) for x in filter(lambda number: not number[1]['marked'], self.bingo.items())]
+        return sum(unmarked) * int(number)
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+with open(filename) as f:
+    lines = f.readlines()
+    calls = lines[0].split(',')
+    raw_boards = [x for x in lines[1:] if x != '\n']
+    board_objs = []
+    for i in range(int(len(raw_boards)/5)):
+        board_objs.append(Board(raw_boards[i*5:(i+1)*5]))
+    while calls:
+        number = calls.pop(0)
+        winning_boards = []
+        for board in board_objs:
+            if board.Mark(number):
+                winning_boards.append(board)
+                if len(board_objs) == 1:
+                    print(board.GetScore(number))
+        for winning_board in winning_boards:
+            board_objs.remove(winning_board)                
